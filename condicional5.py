ec5d8704ea5a5622dd45911fec558fd1265e1000
brenda zitlali lopez barrera <brenlop2708@gmail.com>
2024-11-06 19:14:03 -0600

    condicional 5

---
+++ condicional5.py
@@ -0,0 +1,16 @@
+"""
+un programa que pida un nombre de usuario y una contraseña 
+ y si se ha introducido "pepe" y "asdasd" se indica "Has entrado al sistema", 
+ sino se da un error
+ if expresiones
+         instrucciones
+else:
+    instrucciones
+"""
+usuario =input("ingrese su nombre de usuario:")
+contraseña =input("ingrese su congtraseña:")
+if usuario ==input("ingrese su contraseña:")
+if  usuario == "pepe" and contraseña == "asdasd:"
+print("has entado al sistema")
+else:
+    print("error:usuario o contraseña incorrecto")
