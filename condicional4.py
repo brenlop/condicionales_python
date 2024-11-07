831e8bbf072bc334bccc78805fc2c945933e9804
brenda zitlali lopez barrera <brenlop2708@gmail.com>
2024-11-06 18:52:07 -0600

    condicional 4

---
+++ condicional4.py
@@ -0,0 +1,15 @@
+"""
+un programa que pida al usuario dos numeros y muestre su division
+si el segundo no es cero, o un mensaje de aviso en caso contrario 
+if expresiones
+        instrucciones 
+else: 
+    instrucciones
+    """
+    num1 =float(input("ingrese el dividendo:"))
+    num2 =float(input("ingrese el divisor:"))
+    if num2 ! =0
+    resultado =num1/num2
+    print(f"el resultado de la division es:{resultado }") 
+    else:
+        print("error:no se puede dividir por cero")
