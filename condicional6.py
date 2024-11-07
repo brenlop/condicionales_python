46f12c4ae9feaba910d3acf49fc36418929df2e2
brenda zitlali lopez barrera <brenlop2708@gmail.com>
2024-11-06 19:29:00 -0600

    condicional 6

---
+++ condicional6.py
@@ -0,0 +1,12 @@
+"""
+Programa que lea una cadena por teclado y compruebe si es una letra mayúscula.
+if expresiones 
+        instrucciones
+else:
+    instrucciones
+    """
+cadena =input("ingrese una cadena:")
+if cadena.isuput()and len(cadena:)==1:
+    print("es una letra mayuscula")
+    else:
+        print("no es una letra mayuscula")
