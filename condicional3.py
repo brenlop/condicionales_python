eef0d40b061a94e8663e191fbee12a4a50b31062
brenda zitlali lopez barrera <brenlop2708@gmail.com>
2024-11-06 18:33:17 -0600

    condicional 3

---
+++ condicional3.py
@@ -0,0 +1,13 @@
+"""
+un programa que lea un numero e indique 
+si es par o impar
+if expresiones 
+        instrucciones
+else:
+    instrucciones
+"""
+num =int(input("ingrese un numero:"))
+if num % 2 ==0
+print("el numero es par")
+else:
+    print("el numero es impar")
