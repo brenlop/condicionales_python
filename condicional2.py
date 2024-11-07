a6d0741adead72d2a1a9da4b7c6c847c60f5d1e7
brenda zitlali lopez barrera <brenlop2708@gmail.com>
2024-11-06 18:24:07 -0600

    condicional 2

---
+++ condicional2.py
@@ -0,0 +1,15 @@
+"""
+un programa que pida un numero y diga si es positivo,
+negativo o 0.
+if expresiones
+        instruciones
+else:
+    instrucciones
+"""
+num = float(input("ingrese un numero:"))
+if num > 0:
+    print("el numero es positivo")
+    elif num < 0:
+        print("el numero es negativo")
+        else:
+            prin("el numero es 0")
