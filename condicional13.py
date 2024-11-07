0f9d5eed05befa80856ee153bc18c0f6a4df22f0
brenda zitlali lopez barrera <brenlop2708@gmail.com>
2024-11-07 17:51:34 -0600

    condicional 13

---
+++ condicional13.py
@@ -0,0 +1,28 @@
+"""
+Realiza un programa que pida el dí­a de la semana (del 1 al 7) y escriba 
+el dí­a correspondiente. 
+si introducimos otro número nos da un error.
+if expresiones
+        instrucciones 
+else:
+    instrucciones
+"""
+Else
+            'Mostrar día correspondiente
+            Select Case diaSemana
+                Case 1
+                    Console.WriteLine("El día correspondiente es: Domingo")
+                Case 2
+                    Console.WriteLine("El día correspondiente es: Lunes")
+                Case 3
+                    Console.WriteLine("El día correspondiente es: Martes")
+                Case 4
+                    Console.WriteLine("El día correspondiente es: Miércoles")
+                Case 5
+                    Console.WriteLine("El día correspondiente es: Jueves")
+                Case 6
+                    Console.WriteLine("El día correspondiente es: Viernes")
+                Case 7
+                    Console.WriteLine("El día correspondiente es: Sábado")
+            End Select
+        End If
