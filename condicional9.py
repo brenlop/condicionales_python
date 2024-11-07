a19fc4d9dd4bdd49ad78b971415a863adffccc90
brenda zitlali lopez barrera <brenlop2708@gmail.com>
2024-11-07 16:54:20 -0600

    condicional 9

---
+++ condicional9.py
@@ -0,0 +1,17 @@
+"""
+Programa que pida tres números y los muestre ordenados (de mayor a menor);
+if expresiones 
+        instrucciones
+else:
+    instrucciones
+module program
+sub main()
+"declaracion de variables"
+dim num1, num2, num3 as integer
+"pedir numero al usuario"
+console. write ("ingrese el primer numero:") 
+num1 = convert.tolnt32(console.readline())
+cosole.write("ingrese el segundo numero:")
+num2 = convert.tolnet32(console.readline())
+cosole.write("ingrese el tercer numero:")
+num3 = convert.tolnet32(console.readline())
